1c032cf241187eb7563a4548a5c716ded683e6b3
Shivam Bansal <shivambansal@Shivams-MacBook-Air.local>
2024-05-23 19:31:41 +0530

    add test case for Custom delimiters

--- Incubyte_assesment.py
+++ Incubyte_assesment.py
@@ -3,8 +3,15 @@ import unittest, re
 def add(numbers):
     if not numbers:
         return 0
-    numbers = numbers.replace('\n', ',')
-    return sum(map(int, numbers.split(',')))
+    
+    delimiter = ','
+    if numbers.startswith('//'):
+        parts = numbers.split('\n', 1)
+        delimiter = re.escape(parts[0][2:])
+        numbers = parts[1]
+    
+    numbers = numbers.replace('\n', delimiter)
+    return sum(map(int, re.split(delimiter, numbers)))
 
 class TestStringCalculator(unittest.TestCase):
     '''
@@ -25,12 +32,13 @@ class TestStringCalculator(unittest.TestCase):
     #Case 4: Test New Line
     def test_add_newline_delimiter(self):
         self.assertEqual(add("1\n2,3"), 6)
+
+    #Case 5: Test Custom Deliminator
+    def test_add_custom_delimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
+
+        #Add another test case pipe | (not mention in problem statement)
+        self.assertEqual(add("//|\n1|2|3"), 6)
     
-    
-
-
-
-    
-
 if __name__ == '__main__':
     unittest.main()
